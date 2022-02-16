0451d8e31e61675d5b0b165ec548dc9eb1de2355
Geoluread-dev <geoluread.6541@gmail.com>
2022-02-16 13:10:11 +0100

    Buffetten en BBQ klaar

--- js/script.js
+++ js/script.js
@@ -14,43 +14,9 @@ const navSlide = () => {
     burger.addEventListener('click', () => {
         // Toggle Navigation
         nav.classList.toggle('nav-active');
-        // Animate Links
-        navItems.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navListFade 0.5s ease forwards ${index / 5 + .3}s`;
-            }
-        });
         // Burger Animation
         burger.classList.toggle('toggle');
     });
 };
 
-const dropdownContent = () => {
-    const item = document.querySelector('a');
-    const dropdown = document.querySelector('.dropdown');
-
-    item.addEventListener('mouseover', () => {
-        item.classList.add('item-active');
-
-        if (dropdown.style.display === 'block') {
-            dropdown.style.display = 'none';
-        } else {
-            dropdown.style.display = 'block';
-        }
-    });
-
-    item.addEventListener('mouseleave', () => {
-        item.classList.remove('item-active');
-
-        if (dropdown.style.display === 'block') {
-            dropdown.style.display = 'none';
-        } else {
-            dropdown.style.display = 'block';
-        }
-    });
-};
-
 navSlide();
-dropdownContent();
